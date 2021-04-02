c2b34836d0017d5b428e28959cca9c2cc79bf4ec
Valeria Meza <valeriameza173@gmail.com>
2021-04-02 16:22:29 -0500

    add table template and modify.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NavbarLinksComponent } from './components/navbar-links/navbar-links.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    NavbarLinksComponent
+    NavbarLinksComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
