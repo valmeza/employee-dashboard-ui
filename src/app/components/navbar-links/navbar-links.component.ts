1f53c107e36e956a952cec023a7c62246565e332
Valeria Meza <valeriameza173@gmail.com>
2021-04-02 15:40:54 -0500

    add tailwind config, add navbar component, add navbar links component and style.

---
+++ src/app/components/navbar-links/navbar-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-links',
+  templateUrl: './navbar-links.component.html',
+  styleUrls: ['./navbar-links.component.scss']
+})
+export class NavbarLinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'nunito': 'Nunito'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
